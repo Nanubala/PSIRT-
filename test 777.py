d32d24f0fb920a7bafec8f33936210b295106e33
Madhav <99241414+Nanubala@users.noreply.github.com>
2022-03-10 14:20:37 +0530

    PSIRT LAST.py

--- test 777.py
+++ test 777.py
@@ -2,34 +2,54 @@ from github import Github
 import requests
 import json
 import re
+import base64
 from pprint import pprint
+from pynpm import NPMPackage
+import subprocess
+import os
 
-access_token = ""
 
-g = Github(access_token)
+g=Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+finalvulpackage = ""
+packagedata = ""
+packagelockdata = ""
+
 for repo in g.get_user().get_repos():
-    print("repo name is ::", repo)
+    if (repo.name.__eq__("bms-push-servicebroker")):
+        print("*******", repo.clone_url)
+        files = repo.get_contents("")
+        print("files ::", files)
+        s = repo.get_contents('package.json')
+        packagecontent = s.decoded_content
+        print("package.json content::", packagecontent)
+        s = repo.get_contents('package-lock.json')
+        packagelockcontent = s.decoded_content
+        print("package-lock .json content ::", packagelockcontent)
+        packagedata = json.loads(packagecontent)
+        packagelockdata = json.loads(packagelockcontent)
 
     for issue in repo.get_issues():
-        # print("issue name is ::", issue)
-
-        for label in issue.get_labels():
-            # print("*********issue having label********",label.name)
-            if (label.name.__eq__("psirt")):
-                b=issue.body.replace("<br />", "")
-                b1=b.replace("**","")
+      for label in issue.get_labels():
+        if (label.name.__eq__("psirt")):
+                b = issue.body.replace("<br />", "\n")
+                b1 = b.replace("**", "")
                 dict_1 = json.dumps(b1)
                 b = issue.body.__repr__()
-               # a = re.search('Affected Products:', b)
-                print("*********PSIRT issue********", issue)
+                print("*********PSIRT issue ********", issue)
                 start = b.find("Affected Products:")
                 end = b.find("Dependent Products")
                 substring = b[start:end]
-                print(substring.replace("<br />", ""))
-                content = repo.get_contents("")
-                print("content ::", content)
-                for x in range(len(content)):
-                    if (content[x].__eq__("simple-get")):
-                        print("true")
-                    else:
-                        issue.create_comment("Affected package simple-get 4.0.0 is not used")
+                vulpackage = substring.replace("<br />", "")
+                size = len(vulpackage)
+                print("vulnerability package::",vulpackage[20:size-2])
+                finalvulpackage = vulpackage[20:size-2]
+
+                if finalvulpackage in packagedata:
+                    print("found in package.json",packagedata[finalvulpackage])
+                else:
+                    print("not found in package.json")
+
+                if finalvulpackage in packagelockdata:
+                    print("found in package-lock.json", packagelockdata[finalvulpackage])
+                else:
+                    print("not found in package-lock.json")
